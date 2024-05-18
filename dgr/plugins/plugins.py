a45e975cd4015f804f1c9acb436637b83e7c727e
Chris Zuelsdorf <44318390+chriszuelsdorf@users.noreply.github.com>
2024-05-18 14:39:20 -0500

    add plugin loader

---
+++ dgr/plugins/plugins.py
@@ -0,0 +1,10 @@
+import sys
+import importlib.util
+
+def load_plugin(name, config):
+    fn = config.dirs.base + f"/plugins/{name}.py"
+    spec = importlib.util.spec_from_file_location(name, fn)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[f"dgr__plugin__{name}"] = module
+    spec.loader.exec_module(module)
+    return module
