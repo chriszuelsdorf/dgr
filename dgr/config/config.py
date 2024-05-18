fabb7cae7825a8f5562863db411b4e06935f1901
Chris Zuelsdorf <44318390+chriszuelsdorf@users.noreply.github.com>
2024-05-18 14:22:06 -0500

    use dotdict

--- dgr/config/config.py
+++ dgr/config/config.py
@@ -22,6 +22,6 @@ class Config:
         if not os.path.exists(path):
             src = pathlib.Path(__file__).parent.parent / "blob/dgr-49d7b9af5e2d.yaml"
             shutil.copyfile(src, path)
-        self._conf = load_yaml(path)
+        self._conf = convert_to_dotdict(load_yaml(path))
     def __getattr__(self, name: str) -> os.Any:
         return self._conf[name]
