98fb00f62f37adcb029b5591e220d0ec7d23c157
Chris Zuelsdorf <44318390+chriszuelsdorf@users.noreply.github.com>
2024-05-19 14:26:58 -0500

    add functionality to create dagrun

--- dgr/manager/state/dag.py
+++ dgr/manager/state/dag.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
+import datetime
 
 from ...cron import Cron
 from .task import Task
 from ...types import DotDict
+from .state import State
+
+Q_LATEST_DAGRUN = "SELECT max(\"scheduled\") FROM dagruns WHERE \"name\" = ?;"
+
 
 @dataclass
 class Dag:
@@ -10,3 +15,43 @@ class Dag:
     sch: Cron
     backrun: bool
     tasks: DotDict[str, Task]
+
+    def eligible_for_run(self, state: State) -> bool:
+        # Find time of last run
+        res = state.db.querymany(Q_LATEST_DAGRUN, [self.name])
+
+        # There is a known last run and the last cron ts is later than the last
+        #   known run
+        if len(res) == 1 and self.sch.prev() > res[0][0]:
+            return True
+        # There is no last known run but backrun is set to True
+        elif len(res) == 0 and self.backrun is True:
+            return True
+        return False
+    
+    def create_dagrun(self, state: State):
+        now_ts = datetime.datetime.now(datetime.timezone.utc)
+        # init dagrun
+        stmt = (
+            "INSERT INTO dagruns "
+            '("name", "status", "scheduled", "last_updated") '
+            "VALUES (%s, %s, %s, %s) "
+            'RETURNING "id";'
+        )
+        r = state.db.executefetch(stmt, [self.name, "created", self.sch.prev(), now_ts])
+        dagrun_id = r[0][0]
+        # init tasks
+        taskids = {}
+        taskdeps = {}
+        for taskname, taskspec in self.tasks.items():
+            taskids[taskname] = taskspec.create_task(state, dagrun_id, now_ts)
+            taskdeps[taskname] = taskspec.dep
+        # init taskdeps
+        stmt = (
+            'INSERT INTO taskdeps ('
+            '"task_id", "dep_task_id", "last_updated"'
+            ') VALUES (%s, %s, %s);'
+        )
+        for taskname, deplist in taskdeps.items():
+            for dep in deplist:
+                state.db.executefetch(stmt, [taskids[taskname], taskids[dep], now_ts])
--- dgr/manager/state/task.py
+++ dgr/manager/state/task.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 import typing
+import datetime
 
 from ...types import DotDict
+from .state import State
 
 @dataclass
 class Task:
@@ -9,3 +11,17 @@ class Task:
     plugin: str|None
     args: DotDict[str, typing.Any]
     dep: list[str]
+
+    def create_task(self, state: State, dagrun_id: int, now_ts: datetime.datetime):
+        stmt = (
+            'INSERT INTO tasks ('
+            '"dagrun_id", "name", "plugin", "pluginargs", '
+            '"status", "attempts", "last_updated"'
+            ')'
+            'VALUES (%s, %s, %s, %s, %s, %s, %s);'
+        )
+        vals = [
+            dagrun_id, self.name, self.plugin, str(self.args), "created", 0, now_ts
+        ]
+        r = state.db.executefetch(stmt, vals)
+        return r[0][0]
